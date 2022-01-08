7b20af8e246242ad7daff7b2f95530aadb401797
francisco <fr.betancourt@live.com>
2022-01-08 14:46:20 -0300

    Called WETH contract deposit function for getting WETH.

--- scripts/get_weth.py
+++ scripts/get_weth.py
@@ -14,3 +14,6 @@ def get_weth():
     # Address
     account = get_account()
     weth = interface.IWeth(config["networks"][network.show_active()]["weth_token"])
+    tx = weth.deposit({"from": account, "value": 0.1 * 10 ** 18})
+    print("Received 0.1 WETH")
+    return tx
