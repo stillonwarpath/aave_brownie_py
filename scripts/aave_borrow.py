2a3bcdb4945929e76c62363365a4f0bc1a6fd005
francisco <fr.betancourt@live.com>
2022-01-09 15:45:42 -0300

    Added function for approving ERC20 tokens

--- scripts/aave_borrow.py
+++ scripts/aave_borrow.py
@@ -1,6 +1,10 @@
 from brownie import config, network, interface
 from scripts.get_weth import get_weth
 from scripts.helpful_scripts import get_account
+from web3 import Web3
+
+# 0.1
+ammount = Web3.toWei(0.1, "ether")
 
 
 def main():
@@ -13,9 +17,16 @@ def main():
     lending_pool = get_lending_pool()
     # Approve sending out ERC20 tokens
     # approve_erc20()
+    approve_erc20(amount, lending_pool.address, erc20_address, account)
 
 
-def approve_erc20():
+def approve_erc20(amount, spender, erc20_address, account):
+    print("Approving ERC20 token...")
+    erc20 = interface.IERC20(erc20_address)
+    tx = erc20.approve(spender, amount, {"from": account})
+    tx.wait(1)
+    print("Approved!")
+    return tx
     # ABI
     # Address
     pass
