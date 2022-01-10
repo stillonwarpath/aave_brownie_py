8861c18d315d9b99486be1f32c56f9cb44d15203
francisco <fr.betancourt@live.com>
2022-01-10 18:23:19 -0300

    Added aggregator v3 interface code.

--- scripts/aave_borrow.py
+++ scripts/aave_borrow.py
@@ -26,6 +26,17 @@ def main():
     print("Deposited!")
     # ...how much?
     borrowable_eth, total_debt = get_borrowable_data(lending_pool, account)
+    # 0.1 ETH deposited
+    # 0.08 liquidation threshold
+    print("Let's borrow!")
+    # DAI in terms of ETH
+    dai_eth_price = get_asset_price(
+        config["networks"][network.show_active()]["dai_eth_price_feed"]
+    )
+
+def get_asset_price(price_feed_address):
+    # ABI
+    # Address
 
 
 def get_borrowable_data(lending_pool, account):
